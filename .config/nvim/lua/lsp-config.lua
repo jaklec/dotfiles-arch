a9b9cbe2f82e86ddb081776c608a22e6570bf07d
Jakob Leczinsky <jakob.leczinsky@gmail.com>
2021-09-28 10:12:07 +0200

    nvim: autostart coq

--- .config/nvim/lua/lsp-config.lua
+++ .config/nvim/lua/lsp-config.lua
@@ -1,4 +1,9 @@
 local nvim_lsp = require("lspconfig")
+
+-- Autostart COQ on startup. This must happen before `require("coq")`.
+-- The auto_start parameter can have values [false|true|'shut-up']
+vim.g.coq_settings = { auto_start = 'shut-up' }
+
 local coq = require("coq")
 
 
