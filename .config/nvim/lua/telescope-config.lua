3d74f193930063c66451f866cb6e19ff936f40f7
Jakob Leczinsky <jakob.leczinsky@gmail.com>
2021-10-08 08:33:57 +0200

    nvim: add git search to telescope

--- .config/nvim/lua/telescope-config.lua
+++ .config/nvim/lua/telescope-config.lua
@@ -1,7 +1,10 @@
 local map = vim.api.nvim_set_keymap
 
-options = { noremap = true, silent = true }
+options = {noremap = true, silent = true}
 
 map('n', '<C-p>', ':lua require("telescope.builtin").find_files()<cr>', options)
-map('n', '<leader>s', ':lua require("telescope.builtin").live_grep()<cr>', options)
+map('n', '<leader>s', ':lua require("telescope.builtin").live_grep()<cr>',
+    options)
 map('n', '<leader>b', ':lua require("telescope.builtin").buffers()<cr>', options)
+map('n', '<leader>g', ':lua require("telescope.builtin").git_files()<cr>',
+    options)
