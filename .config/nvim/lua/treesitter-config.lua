fa3e350623d24ca69690e89349eceb341c6218e5
Jakob Leczinsky <jakob.leczinsky@gmail.com>
2021-10-22 10:26:04 +0200

    nvim: temp disable treesitter for cpp which currently seems to be broken

--- .config/nvim/lua/treesitter-config.lua
+++ .config/nvim/lua/treesitter-config.lua
@@ -1,43 +1,18 @@
 local ts_config = require("nvim-treesitter.configs")
 
 ts_config.setup {
-  rainbow = {
-    enable = true,
-    extended_mode = true,
-    max_file_lines = 300,
-  },
-  --ensure_installed = "maintained",
+  rainbow = {enable = true, extended_mode = true, max_file_lines = 300},
+  -- ensure_installed = "maintained",
   ensure_installed = {
-    "javascript",
-    "html",
-    "css",
-    "bash",
-    "lua",
-    "json",
-    "python",
-    "rust",
-    "c",
-    "cmake",
-    "cpp",
-    "dockerfile",
-    "go",
-    "graphql",
-    "java",
-    "regex",
-    "scss",
-    "svelte",
-    "toml",
-    "typescript",
-    "vue",
-    "yaml"
+    "javascript", "html", "css", "bash", "lua", "json", "python", "rust", "c",
+    "cmake", "cpp", "dockerfile", "go", "graphql", "java", "regex", "scss",
+    "svelte", "toml", "typescript", "vue", "yaml"
   },
   highlight = {
     enable = true,
     use_languagetree = true,
-    disable = { "typescript" },
-    additional_vim_regex_highlighting = false,
+    disable = {"typescript", "cpp"},
+    additional_vim_regex_highlighting = false
   },
-  indent = {
-    enable = false,
-  }
+  indent = {enable = false}
 }
