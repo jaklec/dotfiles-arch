bf40c7c195c253ba4405f103cd94b73458ccbe6e
Jakob Leczinsky <jakob.leczinsky@gmail.com>
2021-11-05 10:00:49 +0100

    fish: fzf settings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
   fzf_key_bindings
 
+  set -gx FZF_DEFAULT_COMMAND 'rg --files'
+  set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+  set -gx FZF_ALT_C_COMMAND "fd --type directory . $HOME"
+  set -gx FZF_ALT_C_OPTS "--preview 'tree -C {} | head -200"
+  set -gx FZF_CTRL_T_OPTS "--preview '(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C {}) 2> /dev/null | head -200'"
+
   source ~/.bash_aliases
 end
